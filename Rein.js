f595a809de8c4ddf275bde4f5f8a79c851a0c79e
akeo09 <keoalicia09@gmail.com>
2025-03-13 14:49:53 -0400

    updated modal and bio

---
+++ Rein.js
@@ -0,0 +1,14 @@
+let i = 1;
+for (i = 0; i <= 1; i++) {
+	conole.log(i);
+	if(i > 100) {
+		break;
+	} if (i%3 == 0) {
+	console.log("Rein");
+	} if (i%5 == 0) {
+	console.log("gold");
+	} if (i%15 == 0) {
+	console.log("Reingold");
+	}
+
+}
--- js/main.js
+++ js/main.js
@@ -27,3 +27,34 @@ window.onload = function() {
         window.scrollTo(0, 0);  // Scroll to the top after the page loads
     }, 10);
 };
+
+const challengeContainers = document.querySelectorAll(".project-container.challenges");
+const modal = document.getElementById("modal");
+const modalImage = document.getElementById("modal-image");
+const closeModal = document.getElementById("close-modal");
+
+// Open modal when clicking on a challenge container
+challengeContainers.forEach((container) => {
+    container.addEventListener("click", () => {
+        const img = container.querySelector(".project-image");
+        if (img) { // Make sure image exists
+            const imgSrc = img.getAttribute("src");
+            modalImage.src = imgSrc;
+            modal.style.display = "block";
+        }
+    });
+});
+
+// Close modal on (X) click
+closeModal.addEventListener("click", () => {
+    modal.style.display = "none";
+    modalImage.src = "";
+});
+
+// Close modal when clicking outside modal content
+window.addEventListener("click", (e) => {
+    if (e.target === modal) {
+        modal.style.display = "none";
+        modalImage.src = "";
+    }
+});
